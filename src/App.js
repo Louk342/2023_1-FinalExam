bfea05494e9aee70422284c325b4a26dcf0e7198
BuildTools <unconfigured@null.spigotmc.org>
2023-06-01 13:20:24 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> 가나다라마바사아자차카타파하 </h1>
+      가나다라마바사 아자차카타파하
     </div>
   );
 }
---
+++ src/components/body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Body(){
+    return(
+        <div className='body'>
+            
+        </div>
+    );
+}
+export default Body;
