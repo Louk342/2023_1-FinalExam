4f36175f724004d31d8ec8a958d170a86ffaac9c
BuildTools <unconfigured@null.spigotmc.org>
2023-06-01 11:15:51 +0900

    ?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 가나다라마바사아자차카타파하 </h1>
     </div>
   );
 }
