deee496d70d08a47ca342e6d03b44fb4367bb7ac
BuildTools <unconfigured@null.spigotmc.org>
2023-06-06 23:39:00 +0900

    1

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 function Login(){
-    const [inputId, setInputId] = useState('')
-    const [inputPw, setInputPw] = useState('')
+    let [inputID,setInputID]=useState();
+    let [inputPW,setInputPW]=useState();
+
     return (
-        <div>
-            <h1 style={{color:'white'}}>로그인</h1>
+        <div style={{color:'white'}}>
+            <div style={{borderRadius:'30px',backgroundColor:'#999999',width:'500px',margin:'100px auto',padding:'20px',height:'500px',boxShadow:' 0 4px 8px 0 rgba(0,0,0,.2), 0 6px 20px 0 rgba(0,0,0,.19)'}}>
+                <img src={require('../img/mainLogo.png')} style={{ padding: '10px', width: '100px' }} />
+                <h1>로그인</h1>
+                <div style={{width:'330px',margin:' 10px auto'}}>
+                    <div style={{margin:'10px'}}><TextField fullWidth margin='normal' label="E-mail" variant="filled" inputProps={inputID}/></div>
+                    <div style={{margin:'10px'}}><TextField fullWidth margin='normal' type="Password" label="Password" variant="filled" inputProps={inputPW}/></div>
+                    <span style={{margin:'30px'}}><Button variant='contained' sx={{backgroundColor:'#444444'}}>로그인</Button></span>
+                    <span style={{margin:'30px'}}><Button variant='contained' sx={{backgroundColor:'#444444'}}>회원가입</Button></span>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/register.js
+++ src/components/register.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 function register(){
+    let [inputID,setInputID]=useState();
+    let [inputPW,setInputPW]=useState();
     return (
-        <div>
-            <h1 style={{color:'white'}}>회원가입</h1>
+        <div style={{color:'white'}}>
+            <div style={{borderRadius:'30px',backgroundColor:'#999999',width:'500px',margin:'100px auto',padding:'20px',height:'500px',boxShadow:' 0 4px 8px 0 rgba(0,0,0,.2), 0 6px 20px 0 rgba(0,0,0,.19)'}}>
+                <img src={require('../img/mainLogo.png')} style={{ padding: '10px', width: '100px' }} />
+                <h1>회원가입</h1>
+                <div style={{width:'330px',margin:' 10px auto'}}>
+                    <div style={{margin:'10px'}}><TextField fullWidth margin='normal' label="E-mail" variant="filled" inputProps={inputID}/></div>
+                    <div style={{margin:'10px'}}><TextField fullWidth margin='normal' type="Password" label="Password" variant="filled" inputProps={inputPW}/></div>
+                    <span style={{margin:'30px'}}><Button variant='contained' sx={{backgroundColor:'#444444'}}>회원가입</Button></span>
+                </div>
+            </div>
         </div>
     );
 };
