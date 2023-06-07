83fef86ed08fa7f381b7b99e1e947ce279bc6124
BuildTools <unconfigured@null.spigotmc.org>
2023-06-07 18:11:53 +0900

    1

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const mysql = require('mysql');
 const app = express();
 const PORT = 3001;
-const db = mysql.createPool({
+/* const db = mysql.createPool({
     host: 'louk342.iptime.org',
     user: 'root',
     password: '@Kim9077865',
     database: 'main',
-});
+}); */
 app.use(cors({
     origin:'*',
     credentials:true,
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(PORT,()=>{
     console.log(`server run on ${PORT}`);
 });
-app.get("/api/server", (req, res) => {
+/* app.get("/", (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     
     const sqlQuery = "select * from user";
@@ -26,4 +26,5 @@ app.get("/api/server", (req, res) => {
     db.query(sqlQuery, (err, result) => {
         res.send(result);
     });
-});
+    
+}); */
